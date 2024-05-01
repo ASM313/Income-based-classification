ffd20a7b3b38a88e92a8119dfb007604857693e2
Atiq Mansoori <er_asm@outlook.com>
2024-05-01 11:57:37 +0530

    Data Ingestion Completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Income-based-classification
+
+# Workflows
+
+    update config.yaml
+    update schema.yaml
+    update params.yaml
+    update the entity
+    update the configuration manager in src config
+    update the components
+    update the pipeline
+    update the main.py
+    update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.incomeClassifier import logger
+from src.incomeClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 logger.info("Logging has started")
+
+STAGE_NAME = "Data Ingestion stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
